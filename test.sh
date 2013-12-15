fe8aceda3a25cd8b8c97fe46a08f65a52797e506
lostleaf <jack951753@gmail.com>
2013-12-15 23:05:48 +0800

    add some test script

---
+++ test.sh
@@ -0,0 +1,9 @@
+time java -cp bin nachos.machine.Machine -[] nachos-sjtu/conf/proj5.conf -- nachos.ag.FilesysGrader -x filesys_dir.coff -# output=filesys_dir.out
+time java -cp bin nachos.machine.Machine -[] nachos-sjtu/conf/proj5.conf -- nachos.ag.FilesysGrader -x fs_num.coff -# output=fs_num.out 
+time java -cp bin nachos.machine.Machine -[] nachos-sjtu/conf/proj5.conf -- nachos.ag.FilesysGrader -x filesys_file.coff -# output=filesys_file.out,coffPar0=10240,coffPar1=100 
+time java -cp bin nachos.machine.Machine -[] nachos-sjtu/conf/proj5.conf -- nachos.ag.FilesysGrader -x filesys_link.coff 
+time java -cp bin nachos.machine.Machine -[] nachos-sjtu/conf/proj5.conf -- nachos.ag.FilesysGrader -x filesys_symlink.coff 
+time java -cp bin nachos.machine.Machine -[] nachos-sjtu/conf/proj5.conf -- nachos.ag.FilesysGrader -x filesys_open_unlink.coff 
+time java -cp bin nachos.machine.Machine -[] nachos-sjtu/conf/proj5.conf -- nachos.ag.FilesysGrader -x fs_dir_file.coff 
+time java -cp bin nachos.machine.Machine -[] nachos-sjtu/conf/proj5.conf -- nachos.ag.FilesysGrader -x fs_linuxstyle_dir_op.coff 
+time java -cp bin nachos.machine.Machine -[] nachos-sjtu/conf/proj5.conf -- nachos.ag.FilesysGrader -x fs_size.coff -# output=fs_size.out 
